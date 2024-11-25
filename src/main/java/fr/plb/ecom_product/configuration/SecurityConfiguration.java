3daec76447eb758d7ef02842bb456cb3b28975dc
Cyril Casaucau <casaucau.cyril@protonmail.com>
2024-11-25 22:01:06 +0800

    Security layer

---
+++ src/main/java/fr/plb/ecom_product/configuration/SecurityConfiguration.java
@@ -0,0 +1,31 @@
+package fr.plb.ecom_product.configuration;
+
+import fr.plb.ecom_product.configuration.security.JwtMSAuthenticationConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(authorize ->
+                authorize
+                        .requestMatchers(HttpMethod.GET, "/api/products-shop/**").permitAll()
+                        .requestMatchers("/api/**").authenticated())
+                .csrf(AbstractHttpConfigurer::disable)
+                .oauth2ResourceServer(
+                        oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(new JwtMSAuthenticationConverter()))
+                );
+
+        return httpSecurity.build();
+    }
+}
